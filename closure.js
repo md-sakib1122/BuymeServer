f1ff1239d46e3715816b593cf9e24ad182946497
Sakib Hossen <mdaskib5566@gmail.com>
2024-11-30 15:20:44 +0600

    deleting unnecessary file

--- closure.js
+++
@@ -1,6 +0,0 @@
-// parents variables is accessible from any child or decendents but child's varibale is not accessible from any parent
-// it's called lexical scoping
-const obj = {};
-
-console.dir(obj)
- 
