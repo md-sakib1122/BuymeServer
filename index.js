a4ee874dcc4b634d1c5dcb7c404f5fda2befa609
Sakib Hossen <mdaskib5566@gmail.com>
2025-03-12 19:29:24 +0600

    bug fix 1

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const router = require('./routes/index.js');
 var cookieParser = require('cookie-parser')
 
 const app = express();
+app.use((req, res, next) => {
+   res.setHeader(
+     "Content-Security-Policy",
+     "script-src 'self' 'unsafe-eval';"
+   );
+   next();
+ });
 app.use(cors({
    origin: process.env.FRONTEND_URL,
    credentials: true, 
@@ -16,7 +23,7 @@ app.use(cookieParser(process.env.TOKEN_SECRET_KEY));
 app.use('/api',router);
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
  connectDB().then(() => {     // If database is connected succesfully then only server is started
     app.listen(PORT, ()=>{
